644d7c6df2a110a87c3ff3bff23a34433b6d7f08
Jovani Degante <jovasreyes680@gmail.com>
2024-04-18 10:25:21 -0600

    commit 1_5

---
+++ src/app/store/layout.tsx
@@ -0,0 +1,9 @@
+export default function Layout({children}: {children: React.ReactNode}) {
+    return(
+        <main>
+            <nav>navegacion de las categorias</nav>
+
+            {children}
+        </main>
+    )
+}
