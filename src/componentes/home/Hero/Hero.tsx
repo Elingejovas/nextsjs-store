227eb2552c12239eaf2deadcb907b37625571536
Jovani Degante <jovasreyes680@gmail.com>
2024-04-19 01:00:04 -0600

    commit 2_1

--- src/componentes/home/Hero/Hero.tsx
+++ src/componentes/home/Hero/Hero.tsx
@@ -1,8 +1,12 @@
+
+import styles from './Hero.module.css'
+
 export const Hero = () => {
     return(
-     <section>
+     <section className={styles.Hero}>
      
-     <h1> Hero  </h1>
+     <h1> Future World </h1>
+     <h2> Empowering Your Tomorrow, today!</h2>
      
      </section>
     )
